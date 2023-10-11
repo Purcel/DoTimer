175e65a6457642449f96c07b96848dd6b88bb918
Iulian <iulian.purcell@gmail.com>
2023-10-12 00:59:00 +0300

    Dependencies version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,14 +42,14 @@ android {
 dependencies {
     implementation ("androidx.core:core-ktx:1.12.0")
     implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation ("com.google.android.material:material:1.9.0")
+    implementation ("com.google.android.material:material:1.10.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation ("androidx.activity:activity-ktx:1.7.2")
+    implementation ("androidx.activity:activity-ktx:1.8.0")
 
     implementation ("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.3")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.7.3")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.4")
 
     implementation(project(":dial-picker"))
     implementation(project(":dotimer-billing"))
